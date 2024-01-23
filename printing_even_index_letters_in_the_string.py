042c5f053663776e0e26ab0ecd0690b4ef184f0e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 04:58:14 +0800

    testing if nagwork ang range sa name ko

--- printing_even_index_letters_in_the_string.py
+++ printing_even_index_letters_in_the_string.py
@@ -3,7 +3,11 @@
 # PSEUDO CODE
 # - Printing the sentence/phrase "original string is ---"
 print("The original string is 'Sheena Mae.'")
-
+print("\n")
 
 # - Printing the phase "printing only even index number"
+print("Printing the even index letters in the string.")
 # - actual code
+name = "Sheena Mae"
+for i in range(10):
+    print(name[i])
