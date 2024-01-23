8afeb7a2df136297892a287cabe8cd42098a079e
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-24 04:55:05 +0800

    printing the 'original string is --'

---
+++ printing_even_index_letters_in_the_string.py
@@ -0,0 +1,9 @@
+
+
+# PSEUDO CODE
+# - Printing the sentence/phrase "original string is ---"
+print("The original string is 'Sheena Mae.'")
+
+
+# - Printing the phase "printing only even index number"
+# - actual code
