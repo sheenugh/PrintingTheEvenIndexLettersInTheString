69be68a9ed043d56476c519c1f0521554862cfee
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-28 18:36:15 +0800

    nagchange ng program kasi mali yong una

--- printing_even_index_letters_in_the_string.py
+++ printing_even_index_letters_in_the_string.py
@@ -1,29 +1,33 @@
 
 
+
 # >>>>>>>>>> PSEUDO CODE <<<<<<<<<<
 # ---------- ACTUAL CODES ----------
-# - Printing the sentence as "original string is ...."
-print("The original string is 'Sheena Mae.'")
+# - Printing the sentence as "original string is .... and the word inputted"
+characters = input("Enter a Word: ")
+print("The original string is", characters)
 print("\n")
+
+# - Variable
+characters_length = len(characters)
 
 # - Printing the sentence "printing only even index number"
 print("Printing the even index letters in the string.")
 
 # This section is for looping na.
-name = "Sheena Mae"
-for i in range(0, 10, 2):
-    print(name[i])
+for i in range(0, characters_length - 1, 2):
+    print(characters[i])
     
     
-print("\n")
-print("---------------")
-print("OR")
-print("---------------")
-print("\n")
+# print("\n")
+# print("---------------")
+# print("OR")
+# print("---------------")
+# print("\n")
 
-# This section is for looping na ibang method ang pagconstruct ng function/program.
-for i in range(10):
-    if (i%2) == 0:
-        print(name[i])
+# # This section is for looping na ibang method ang pagconstruct ng function/program.
+# for i in range(10):
+#     if (i%2) == 0:
+#         print(name[i])
         
 # Thus the even index letters in the string are S,e,n,,<space>, and a.
