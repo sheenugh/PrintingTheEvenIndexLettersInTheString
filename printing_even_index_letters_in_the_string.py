1b7d28fe4a256b50bc7492e48c43c74bbf580599
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-28 18:49:24 +0800

    finalization ng code

--- printing_even_index_letters_in_the_string.py
+++ printing_even_index_letters_in_the_string.py
@@ -15,19 +15,6 @@ characters_length = len(characters)
 print("Printing the even index letters in the string.")
 
 # This section is for looping na.
-for i in range(0, characters_length - 1, 2):
+for i in range(0, characters_length, 2):
     print(characters[i])
-    
-    
-# print("\n")
-# print("---------------")
-# print("OR")
-# print("---------------")
-# print("\n")
 
-# # This section is for looping na ibang method ang pagconstruct ng function/program.
-# for i in range(10):
-#     if (i%2) == 0:
-#         print(name[i])
-        
-# Thus the even index letters in the string are S,e,n,,<space>, and a.
